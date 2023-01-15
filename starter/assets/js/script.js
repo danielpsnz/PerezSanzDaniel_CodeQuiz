c4086d0dc0efa9cc0ce07a8e8641c598f7e591b0
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-01-15 01:50:08 +0000

    added js file with an array of questions

---
+++ starter/assets/js/script.js
@@ -0,0 +1,48 @@
+var score = document.querySelector("scores");
+var start = document.getElementById("start");
+
+var question_title = document.getElementById("question-title");
+var choices = document.getElementById("choices");
+
+var finalScore = document.getElementById("final-score");
+
+// QUESTIONS
+
+var quizQuestions = [
+    {
+    "question-title" : "", 
+    "one" : "1.",
+    "two" : "2. ",
+    "three" : "3. ",
+    "four" : "4.",
+    "correct" : "",
+    },{
+    "question-title" : "", 
+    "one" : "1.",
+    "two" : "2. ",
+    "three" : "3. ",
+    "four" : "4.",
+    "correct" : "",
+    }, {
+    "question-title" : "", 
+    "one" : "1.",
+    "two" : "2. ",
+    "three" : "3. ",
+    "four" : "4.",
+    "correct" : "",
+    }, {
+    "question-title" : "", 
+    "one" : "1.",
+    "two" : "2. ",
+    "three" : "3. ",
+    "four" : "4.",
+    "correct" : "",
+    }, {
+    "question-title" : "", 
+    "one" : "1.",
+    "two" : "2. ",
+    "three" : "3. ",
+    "four" : "4.",
+    "correct" : "",
+    }
+  ]
