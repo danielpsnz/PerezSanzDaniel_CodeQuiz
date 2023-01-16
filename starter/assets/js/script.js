1767140e9ef74bd21a33f431fd26e5a0ffa39e75
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-01-16 03:41:14 +0000

    added js code with some issues in the events

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -8,11 +8,17 @@ var start = document.getElementById("start");
 
 var questions = document.getElementById("questions");
 var question_title = document.getElementById("question-title");
-var choices = document.getElementById("choices");
+var choice1 = document.getElementById("one");
+var choice2 = document.getElementById("two");
+var choice3 = document.getElementById("three");
+var choice4 = document.getElementById("four");
+var choice5 = document.getElementById("five");
+var questionButton = document.querySelector(".questionButton");
 
 var end_screen = document.getElementById("end-screen");
 var finalScore = document.getElementById("final-score");
 var submit = document.getElementById("submit");
+var initials = document.getElementById("initials"); 
 
 var feedback = document.getElementById("feedback");
 
@@ -66,32 +72,9 @@ function codeQuiz() {
   scores.style.display = "block"; // Shows score
   timer.style.display = "block"; // Shows timer
   wrapper.style.display = "block"; // Shows rules
-  questions.style.display = "hide"; // Hide Questions
-  end_screen.style.display = "hide"; // Hide Final Score pages
+  questions.style.display = "none"; // Hide Questions
+  end_screen.style.display = "none"; // Hide Final Score pages
 }
-
-// Quiz starts 
-function startQuiz() { 
-  wrapper.style.display = "none"; // Hide Rules 
-  questions.style.display = "block"; // Show Quiz Questions Page
-  
-  secondsLeft = 90; // seconds in Timer 
-  
-  var timerInterval = setInterval(function() { 
-    secondsLeft--;
-    time = secondsLeft;
-    if (secondsLeft === 0 || quizQuestions.length === questionIndex) {
-      clearInterval(timerInterval);
-      showFinalScore();
-    }
-  }, 100);
-}
-
-// START BUTTON - DOES NOT WORK
-start.addEventListener("click", function() { 
-  startQuiz()
-  console.log("start")
-})
 
 // Reset global variables when quiz restarts
 function resetVariables() {
@@ -99,6 +82,22 @@ function resetVariables() {
   questionIndex = 0;
 }
 
+// Quiz starts 
+function startQuiz() { 
+  start_screen.style.display = "none"; // Hide Rules 
+  questions.style.display = "block"; // Show Quiz Questions Page
+  
+  secondsLeft = 90; // seconds in Timer 
+  
+  var timerInterval = setInterval(function() { 
+    secondsLeft--;
+    time.textContent = secondsLeft;
+    if (secondsLeft === 0 || quizQuestions.length === questionIndex) {
+      clearInterval(timerInterval);
+      showFinalScore();
+    }
+  }, 1000);
+}
 
   // Questions
 function showQuestions() {
@@ -134,3 +133,95 @@ choice4.addEventListener("click", function (event) {
 choice5.addEventListener("click", function (event) {
   checkAnswer(event);
 })
+
+ // Check if the answer is correct
+ function checkAnswer(event) {
+  event.preventDefault();
+
+  var answer = event.currentTarget.dataset.answer;
+  var correctAnswer = null;
+
+  if (quizQuestions[questionIndex].correct === answer) {
+      correctAnswer = answer;
+  }
+  if (answer === correctAnswer) {
+  answerResponse.textContent = "Correct!"; // If correct, say correct
+  } else {
+  answerResponse.textContent = "Wrong!"; // If wrong, say wrong & deduct 10 points
+      secondsLeft -= 10
+      if (secondsLeft < 0) {
+          secondsLeft = 0;
+      }
+  }
+  if (quizQuestions.length === questionIndex+1) {
+    showFinalScore(); // If it has gone through all questions, show final score
+    return; // If not, print the next question
+  }
+  questionIndex++;
+  showQuestions();
+}
+
+// GO TO "ALL DONE" PAGE AND SHOW FINAL SCORE
+function showFinalScore() { //Function to go to page when time out or quiz complete 
+  questions.style.display = "none"; // Hide Questions
+  end_screen.style.display = "block"; // Shows Final Score pages
+  finalScore.textContent = secondsLeft; // Shows Final score
+} // end of showFinalScore
+
+var highScoreArray = [] // Global variable 
+
+// SHOWS ALL HIGH SCORES 
+function showHighScores() {
+  scores.style.display = "block"; // Display scores
+  timer.style.display = "none"; // Hide timer
+  wrapper.style.display = "none" // Hide first page
+  questions.style.display = "none" // Hide questions
+  end_screen.style.display = "block" // Hide end_screen
+  
+  var getInitials = document.getElementById("initials").value; // captures the value of the initials 
+
+  var highScoreArray = JSON.parse(localStorage.getItem("highScore")) || [];
+  
+  var localStorageArray = { score: secondsLeft, initials: getInitials };
+  highScoreArray.push(localStorageArray)
+  localStorage.setItem("highScore", JSON.stringify(highScoreArray)); // Adds array 
+
+  var highScores = getInitials + ": " + secondsLeft; // add in + getInitials when read it
+
+  $("#highScoreList").append(highScores) // Appends high score & initials
+}
+
+// START BUTTON
+start.addEventListener("click", function() { 
+  startQuiz();
+  console.log("start")
+})
+
+// CLICK TO VIEW HIGH SCORES - DOES NOT WORK 
+scores.addEventListener("click", function() {
+  showHighScores();
+  console.log("view high scores")
+})
+
+// CLICK INTIAL BUTTON TO SHOW HIGH SCORES - WORKS
+submit.addEventListener("click", function() { 
+  showHighScores();
+  console.log("Submit")
+}) 
+
+// CLEAR HIGH SCORES - WORKS
+clearHighScore.addEventListener("click", function() {
+  localStorage.clear();
+})
+
+// GO BACK BUTTON EVENT liSTENER - WORKS 
+goBack.addEventListener("click", function() { // Go back to the home page
+  $("#highScoreList").empty() // clears out container
+  $("#initialInput").val("") // clears out the value in initial input 
+  resetVariables()
+  codeQuizChallenge();
+  console.log("restart quiz")
+})
+
+// Page starts at home page 
+codeQuizChallenge(); 
